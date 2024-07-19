9e280e441c985e2e27ea53ab860aab6523372edd
einerzg <einerzg@gmail.com>
2024-07-19 08:20:21 -0500

    Add TransactionsPayload in the params for endpoint spec

--- lib/rpc/endpoints/spec.ex
+++ lib/rpc/endpoints/spec.ex
@@ -3,10 +3,10 @@ defmodule Soroban.RPC.Endpoint.Spec do
   Specifies the callbacks to build the Soroban's endpoints.
   """
 
-  alias Soroban.RPC.{Error, EventsPayload, HTTPError, Server}
+  alias Soroban.RPC.{Error, EventsPayload, HTTPError, Server, TransactionsPayload}
 
   @type response :: {:ok, struct()} | {:error, Error.t() | HTTPError.t()}
-  @type params :: String.t() | EventsPayload.t() | keyword() | nil
+  @type params :: String.t() | EventsPayload.t() | keyword() | nil | TransactionsPayload.t()
 
   @callback request(server :: Server.t(), params :: params()) :: response()
 end
